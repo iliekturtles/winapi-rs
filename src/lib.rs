af64d1e0f9f269f16d230e4a5f8bdf31e67cffa2
Mike Boutin <mike.boutin@gmail.com>
2015-06-21 14:12:00 -0400

    Implement EncodeUtf16 trait.

--- src/lib.rs
+++ src/lib.rs
@@ -287,6 +287,23 @@ macro_rules! BITFIELD {
     }
 }
 //-------------------------------------------------------------------------------------------------
+// Traits
+//-------------------------------------------------------------------------------------------------
+pub trait EncodeUtf16 {
+    fn encode_utf16(&self) -> Vec<u16>;
+    fn encode_utf16_null(&self) -> Vec<u16>;
+}
+impl<T> EncodeUtf16 for T where T: AsRef<::std::ffi::OsStr> {
+    fn encode_utf16(&self) -> Vec<u16> {
+        use std::os::windows::ffi::{OsStrExt};
+        self.as_ref().encode_wide().collect()
+    }
+    fn encode_utf16_null(&self) -> Vec<u16> {
+        use std::os::windows::ffi::{OsStrExt};
+        self.as_ref().encode_wide().chain(Some(0)).collect()
+    }
+}
+//-------------------------------------------------------------------------------------------------
 // Modules
 //-------------------------------------------------------------------------------------------------
 pub mod audioclient;
